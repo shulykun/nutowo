0caeea6d6602694ddecd049c456ebd633a4aa466
Shulginow <vadim.shulginov@yandex.ru>
2021-01-08 23:26:16 +1000

    Версия 0.1.0

--- nutowo/__init__.py
+++ nutowo/__init__.py
@@ -41,6 +41,7 @@ def get_th(num, prev_number):
 
 def num2text_noun_part(input_num):
     """
+        [Версия для существительных]
         Перевод в текст чисел имеющие в составе не более трех цифр.
         Компонент функции num2text_noun(), которая мз таких троек собирает
         сколь угодно большие числа.
@@ -63,6 +64,36 @@ def num2text_noun_part(input_num):
             pass
 
 
+def num2text_adj_part(input_num):
+    """
+        [Версия для прилагательных]
+        Перевод в текст чисел имеющие в составе не более трех цифр.
+        Компонент функции num2text_noun(), которая мз таких троек собирает
+        сколь угодно большие числа.
+    """
+
+    input_len = len(input_num)
+
+    for j in range(input_len):
+        a = input_num[j:]
+        if a in NUM_ADJ.keys():
+            try:
+                yield NUM_ADJ[a]
+                break
+            except:
+                pass
+
+    for i in range(input_len-j+1,input_len+1):
+        x =  input_num[-i][0] + '0' * (i-1)
+        if x in NUM_NOUN.keys():
+            try:
+                yield NUM_NOUN[x]
+            except:
+                pass
+
+
+
+
 def modify_element(el):
     if el[0] == 'ноль':
         el = []
@@ -83,6 +114,35 @@ def num2text_noun(input_num):
         t = list(num2text_noun_part(i))
         t.reverse()
 
+        element_add = [*t, get_th(size,  t[-1])]
+        element_add = modify_element(element_add)
+
+        r = [*r,*element_add]
+
+        r = [i for i in r if len(i)]
+        size -= 1
+
+    r = ' '.join(r).strip()
+    r = r.replace('один тыс','одна тыс').replace('два тыс','две тыс').split()
+
+    return r
+
+
+def num2text_adj(input_num):
+    """
+        Перевод числа в текст - прилагательные
+    """
+    r = []
+    input_num_l = format(int(input_num), ',').split(',')
+    size = len(input_num_l)
+    for i in input_num_l:
+
+        if size == 1:
+            t = list(num2text_adj_part(i))
+        else:
+            t = list(num2text_noun_part(i))
+
+        t.reverse()
         element_add = [*t, get_th(size,  t[-1])]
         element_add = modify_element(element_add)
 
